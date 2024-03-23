fe5613cb794802b70c450f446492b88006843919
EpicGazel <zaneshark@gmail.com>
2024-03-23 05:44:48 -0400

    Initial work to convert and send by existing links.

--- app.py
+++ app.py
@@ -1,17 +1,52 @@
-from flask import Flask, send_file
+from flask import Flask, request, send_file
 from flask_caching import Cache
 from PIL import Image
 import pillow_avif
 import os
 import io
 import datetime
+import requests
+import urllib.parse
 
 app = Flask(__name__)
 
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
+
+# Define the route for serving the converted image
+@app.route('/convert', methods=['GET'])
+def convert_image():
+    # Get the image URL from the query parameters
+    image_url = request.args.get('url')
+    if len(request.args) > 1:
+        for key, value in request.args.items():
+            image_url += f"&{key}={value}"
+
+    # Check if the URL is provided
+    if not image_url:
+        return 'Image URL is required', 400
+
+    try:
+        # Download the image from the provided URL
+        response = requests.get(image_url)
+        response.raise_for_status()  # Raise an exception for invalid response
+
+        # Open the downloaded image with PIL
+        img = Image.open(io.BytesIO(response.content))
+
+        # Convert the image to webp format
+        webp_buffer = io.BytesIO()
+        img.save(webp_buffer, format='WEBP')
+        webp_buffer.seek(0)
+
+        # Return the converted image
+        return send_file(webp_buffer, mimetype='image/webp')
+    except Exception as e:
+        return str(e), 500
+
+
 # Define the route for serving the image
-@app.route('/<image_name>.webp')
+@app.route('/local/<image_name>.webp')
 def serve_image(image_name):
     # Check if the image with the given name exists in avif format
     avif_path = f'./images/{image_name}.avif'
@@ -19,7 +54,7 @@ def serve_image(image_name):
         # Convert the avif image to webp
         webp_data = convert_image(avif_path)
         return send_file(io.BytesIO(webp_data), mimetype='image/webp')
-        #return send_file(avif_path, mimetype='image/avif')
+        # return send_file(avif_path, mimetype='image/avif')
     else:
         return 'Image not found', 404
 
@@ -33,6 +68,7 @@ def convert_image(input_path):
         timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         print(f"Cache generated at: {timestamp}")
         return output_buffer.getvalue()
+
 
 # Function to convert image format and store on disk
 # def convert_image(input_path, output_path):
